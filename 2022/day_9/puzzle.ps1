21685c2bb24a216c1a1e17874d6f185829becfdb
--///--//-- <14398606+s9rA16Bf4@users.noreply.github.com>
2022-12-10 22:40:08 +0100

    dubbeltake on day 9

--- 2022/day_9/puzzle.ps1
+++ 2022/day_9/puzzle.ps1
@@ -16,11 +16,30 @@ if (-not(Test-Path -Path $file -PathType Leaf)) {
 
 $guts = Get-Content $file
 $guts = $guts -split " "
-$head = [System.Tuple]::Create(0,0)
-$tail = [System.Tuple]::Create(0,0)
-$history = [System.Collections.ArrayList]@()
+$head = [System.Tuple]::Create(0,0) # Start position
+$tail = [System.Tuple]::Create(0,0) # Start position
+$history = [System.Collections.ArrayList]@() # This will keep where the tail has been
 
-function Main(){
+function DirectionHandler{
+    [Parameter ()] [String] $move_set
+    [Parameter ()] [System.Tuple] $tuple_obj
+
+    $tuple_x = [int]$tuple_obj.Item1
+    $tuple_y = [int]$tuple_obj.Item2
+
+    switch ($move_set) {
+        "R" { $tuple_x++ } 
+        "L" { $tuple_x-- } 
+        "U" { $tuple_y++ } 
+        "D" { $tuple_y-- } 
+    }    
+
+    $tuple_obj = [System.Tuple]::Create($tuple_y, $tuple_x)
+
+    return $tuple_obj
+}
+
+function Main{
     $amount = -1
     $direction = ""
     
@@ -33,14 +52,19 @@ function Main(){
             $amount = [int]$guts[$i]
         }
     
-        if ($amount -ne 0){
-        
+        if ($amount -ne 0){ # We are now ready to operate
+            while($amount -gt 0){
+                $commands = @{
+                    move_set=$direction
+                    tuple_obj=$head
+                }
+                DirectionHandler @commands
                 $amount--
             }
+            
         }
     }
-    
-    #$history
+
     $tail_movement = $history.count
     "[PART 1] Tail moved to a total of $tail_movement cells"
 }
